ba8b48d5f6940ab1d85271be1fe6185a29591a31
A_J_Olly <aaronjayoliver@gmail.com>
2019-11-04 13:05:55 +0000

    stop users returning back to the survey page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
+import { AuthGuardService } from './auth-guard.service';
 import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { LocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +16,14 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
+    private location: LocationStrategy,
+    private authGuardService: AuthGuardService,
   ) {
+    this.location.onPopState(() => {
+      this.authGuardService.backClicked = true;
+      return false;
+    });
+
     this.initializeApp();
   }
 

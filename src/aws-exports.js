c00032f230cbb84c5984de48713cccfc99683dfb
A_J_Olly <aaronjayoliver@gmail.com>
2020-04-28 12:21:13 +0100

    built new master environment

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,13 +2,13 @@
 
 const awsmobile = {
     "aws_project_region": "eu-west-2",
-    "aws_appsync_graphqlEndpoint": "https://6nf2imrocffern32nzgvcixm6a.appsync-api.eu-west-2.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://5w5heh54zzh3hmunuskdarasia.appsync-api.eu-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "eu-west-2",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
-    "aws_cognito_identity_pool_id": "eu-west-2:c78145bf-c55e-4044-acc7-67e2071d830f",
+    "aws_cognito_identity_pool_id": "eu-west-2:b01ebd25-1a95-458d-a8b1-3a712d013a95",
     "aws_cognito_region": "eu-west-2",
-    "aws_user_pools_id": "eu-west-2_Ltt6mQem0",
-    "aws_user_pools_web_client_id": "3ibr5krf0k6pn92utvo83fq34i",
+    "aws_user_pools_id": "eu-west-2_KfnKWh1q4",
+    "aws_user_pools_web_client_id": "3vbsoqjc61ok4ftr69fd299qdv",
     "oauth": {}
 };
 

0da3e8ec19d017dccc4b18c4db7379c18e98cb33
user171561 <user171561@92-63-141-240.rdns.melbourne.co.uk>
2020-06-16 14:30:05 +0100

    Successfuly built and deployed app on iOS

--- src/app/home/cta/cta.component.ts
+++ src/app/home/cta/cta.component.ts
@@ -1,7 +1,7 @@
-import { MenuController, AlertController, Platform } from '@ionic/angular';
+import { AlertController, Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AuthService } from './../../services/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
 @Component({

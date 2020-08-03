1ffaae98acd5fd0236dcee11e42a0552042e52b1
neutrify <neutrify@gmail>
2020-08-03 11:59:48 +0100

    Added feature toggle for download app badges. Added app description on cta.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   gaTrackingId: 'UA-165071248-1',
   refreshTimeLimit: 300,
   appleStoreUri: 'https://example.com',
-  googleStoreUri: 'https://play.google.com/store/apps/details?id=news.neutrify.app'
+  googleStoreUri: 'https://play.google.com/store/apps/details?id=news.neutrify.app',
+  readyToRelease: false
 };
